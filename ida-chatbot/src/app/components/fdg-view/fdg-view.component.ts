7479277e0cad47bca5f473ae24b54e51ec55ffba
nikit91@gmail.com <nikit91@gmail.com>
2018-06-08 21:11:40 +0200

    Compilation Error Fix and Dynamic Id passing logic for D3

--- ida-chatbot/src/app/components/fdg-view/fdg-view.component.ts
+++ ida-chatbot/src/app/components/fdg-view/fdg-view.component.ts
@@ -1,28 +1,37 @@
-import {AfterViewInit, Component, OnInit} from '@angular/core';
+import {AfterViewChecked, AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
+import {Message} from '../../models/message';
+
+declare var d3;
+declare function createV4SelectableForceDirectedGraph(a, b);
+
 @Component({
   selector: 'app-fdg-view',
   templateUrl: './fdg-view.component.html',
   styleUrls: ['./fdg-view.component.css']
 })
 export class FdgViewComponent implements OnInit, AfterViewInit {
+  @Input('idpf')
+  public idpf: number;
 
-  constructor() { }
+  public fdgid: string;
+
+  constructor() {
+  }
 
   ngOnInit() {
+    this.fdgid = 'fdg' + this.idpf;
   }
 
   ngAfterViewInit() {
 
-   /* var svg = d3.select('#d3_selectable_force_directed_graph');
-
-    d3.json('/assets/fdg/demo.json', function(error, graph) {
+    const svg = d3.select('#' + this.fdgid);
+    d3.json('/assets/fdg/demo.json', function (error, graph) {
       if (!error) {
-        //console.log('graph', graph);
         createV4SelectableForceDirectedGraph(svg, graph);
       } else {
         console.error(error);
       }
-    });*/
+    });
   }
 
 }
