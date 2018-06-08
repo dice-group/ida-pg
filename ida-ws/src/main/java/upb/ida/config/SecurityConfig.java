4d6bd1f33d35567c9a15ec08ff893fb505995437
nikit91@gmail.com <nikit91@gmail.com>
2018-06-08 15:58:49 +0200

    Handling CORS Issue

---
+++ ida-ws/src/main/java/upb/ida/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package upb.ida.config;
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class SecurityConfig {
+	// Doesn't Work
+	/*@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		final CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList("*"));
+		configuration.setAllowedMethods(Arrays.asList("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH"));
+		// setAllowCredentials(true) is important, otherwise:
+		// The value of the 'Access-Control-Allow-Origin' header in the response must
+		// not be the wildcard '*' when the request's credentials mode is 'include'.
+		configuration.setAllowCredentials(true);
+		// setAllowedHeaders is important! Without it, OPTIONS preflight request
+		// will fail with 403 Invalid CORS request
+		configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}*/
+}
--- ida-ws/src/main/java/upb/ida/rest/MessageRestController.java
+++ ida-ws/src/main/java/upb/ida/rest/MessageRestController.java
@@ -1,22 +1,28 @@
 package upb.ida.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import upb.ida.bean.ResponseBean;
+
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/message")
 public class MessageRestController {
-
+	@Autowired
+	ResponseBean response;
 	@RequestMapping("/sayhello")
 	public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return "Hello " + name + "!";
 	}
 	
 	@RequestMapping("/sendmessage")
-	public String sendmessage(@RequestParam(value = "msg") String msg) {
-		System.out.println(msg);
-		return "Service under development. Please try later.";
+	public ResponseBean sendmessage(@RequestParam(value = "msg") String msg) {
+		response.setPayload("Service under development. Please try later.");
+		return response;
 	}
 
 }
