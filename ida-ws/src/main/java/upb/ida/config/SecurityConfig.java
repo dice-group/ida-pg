7c5e9f3d7aad782f2966df21d9998e80a6d1bdb2
nikit91@gmail.com <nikit91@gmail.com>
2018-06-13 13:23:54 +0200

    Dependency version changes

--- ida-ws/src/main/java/upb/ida/config/SecurityConfig.java
+++ ida-ws/src/main/java/upb/ida/config/SecurityConfig.java
@@ -1,12 +1,6 @@
 package upb.ida.config;
 
-import java.util.Arrays;
-
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class SecurityConfig {
