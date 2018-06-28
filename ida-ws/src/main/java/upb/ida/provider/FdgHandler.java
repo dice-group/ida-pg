09ccea1341063c4f873a512437a161674b1f3d06
faisal07m <faisl07m@gmail.com>
2018-06-28 10:28:03 -0700

    connecting fdg backend to Frontend

--- ida-ws/src/main/java/upb/ida/provider/FdgHandler.java
+++ ida-ws/src/main/java/upb/ida/provider/FdgHandler.java
@@ -12,8 +12,11 @@ import com.rivescript.macro.Subroutine;
 import upb.ida.bean.ResponseBean;
 import upb.ida.constant.IDALiteral;
 import upb.ida.fdg.FDG_Util;
+import upb.ida.temp.DemoMain;
 @Component
 public class FdgHandler implements Subroutine {
+	@Autowired
+	DemoMain DemoMain;
 	@Autowired
 	FDG_Util FDG_Util;
 	@Autowired
@@ -22,10 +25,13 @@ public class FdgHandler implements Subroutine {
 		
 		//		String user = rs.currentUser();
 		try {
+			String actvTbl = (String) responseBean.getPayload().get("actvTbl");
+			String actvDs = (String) responseBean.getPayload().get("actvDs");
+			String path = DemoMain.getFilePath(actvDs,actvTbl );
 			Map<String, Object> dataMap = responseBean.getPayload();
 			dataMap.put("label", "Fdg Handler");
 //			dataMap.put("dsName", message);
-			dataMap.put("dataset", FDG_Util.generateFDG("city//citydistance.csv",args[0].toLowerCase(),args[1].toLowerCase(),args[2].toLowerCase()));
+			dataMap.put("dataset", FDG_Util.generateFDG(path,args[0].toLowerCase(),args[1],args[2]));
 			responseBean.setPayload(dataMap);
 			responseBean.setActnCode(IDALiteral.UIA_FDG);
 			return "pass";
