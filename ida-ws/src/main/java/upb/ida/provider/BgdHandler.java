68c85e9eaf606759fbc66ba721325789474815e4
faisal07m <faisl07m@gmail.com>
2018-06-28 05:44:59 -0700

    saving changes before merge

--- ida-ws/src/main/java/upb/ida/provider/BgdHandler.java
+++ ida-ws/src/main/java/upb/ida/provider/BgdHandler.java
@@ -27,7 +27,7 @@ public class BgdHandler implements Subroutine {
 			Map<String, Object> dataMap = new HashMap<String, Object>();
 			dataMap.put("label", "Bar Graph");
 //			dataMap.put("dsName", message);
-			dataMap.put("dataset", DemoMain.getJsonData("C:\\Users\\Faisal Mahmood\\Desktop\\dice-ida\\ida-ws\\src\\main\\webapp\\city\\citydistance.csv",args[0].toLowerCase(),args[1].toLowerCase()));
+			dataMap.put("dataset", DemoMain.getJsonData("city\\citydistance.csv",args[0].toLowerCase(),args[1].toLowerCase()));
 			responseBean.setPayload(dataMap);
 			responseBean.setActnCode(IDALiteral.UIA_BG);
 			return "pass";
