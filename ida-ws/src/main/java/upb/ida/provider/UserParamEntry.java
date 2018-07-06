e5ffc6223a34f3d78f19fae60d1727a8a373c574
faisal07m <faisl07m@gmail.com>
2018-07-06 08:00:25 -0700

    improving params handler

--- ida-ws/src/main/java/upb/ida/provider/UserParamEntry.java
+++ ida-ws/src/main/java/upb/ida/provider/UserParamEntry.java
@@ -19,7 +19,7 @@ import upb.ida.util.DataDumpUtil;
 import upb.ida.bean.cluster.ClusterParam;
 
 @Component
-public class ParamsHandler implements Subroutine {
+public class UserParamEntry implements Subroutine {
 	
 	private ResponseBean responseBean;
 	@Autowired
@@ -30,29 +30,9 @@ public class ParamsHandler implements Subroutine {
 		//		String user = rs.currentUser();
 		try {
  
-			List<ClusterParam> paramList=  new ArrayList<>();
-            paramList=DataDumpUtil.getClusterAlgoParams(args[0]);
-            int x=0;
-            String algoStr = null;
-            if(paramList.get(0).isOptional()==false) {
-            algoStr="<br>"+paramList.get(0).getName();
-            }
-            else
-            {
-            	algoStr="<br>"+paramList.get(0).getName()+"[Optional]";
-            }
-            
-            for (int i = 0; i < paramList.size()-1; i++) {
-            	if(paramList.get(i+1).isOptional()) {
-            	algoStr=algoStr+"<br>"+paramList.get(i+1).getName()+"[Optional]";
-            	}
-            	else {
-            		algoStr=algoStr+"<br>"+paramList.get(i+1).getName();
-            		
-            	}
-            }
+			
           
-			return algoStr;
+			return "";
 		} catch (NumberFormatException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
