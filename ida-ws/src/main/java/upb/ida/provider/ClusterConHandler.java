7c966824e6406e42b81536c38e7b9d9c1f8c0407
faisal07m <faisl07m@gmail.com>
2018-07-07 07:44:17 -0700

    Implemented subroutine to get algo Names and display them to the user in rive

--- ida-ws/src/main/java/upb/ida/provider/ClusterConHandler.java
+++ ida-ws/src/main/java/upb/ida/provider/ClusterConHandler.java
@@ -16,35 +16,18 @@ public class ClusterConHandler implements Subroutine {
 	@Autowired
 	private DataDumpUtil DataDumpUtil;
 	public String call (com.rivescript.RiveScript rs, String[] args) {
-
 		
-		//		String user = rs.currentUser();
 		try {
-//			String actvTbl = (String) responseBean.getPayload().get("actvTbl");
-//			String actvDs = (String) responseBean.getPayload().get("actvDs");
-//			String actvScrId = (String) responseBean.getPayload().get("actvScrId");
+
             List<String> algoList=  new ArrayList<>();
             algoList=DataDumpUtil.getClusteringAlgoNames();
             String algoStr=new String();
             algoStr= "<br>"+algoList.get(0);
             for (int i = 0; i < algoList.size()-1; i++) {
-            	
-    			algoStr=algoStr+"<br>"+algoList.get(i+1);
+    			
+            	algoStr=algoStr+"<br>"+algoList.get(i+1);
     		}
-            
-            
-			//			Map<String, Object> dataMap = new HashMap<String,Object>();
-//			//dataMap.put("label", "Bar Graph");
-//			//String path = DemoMain.getFilePath(actvDs,actvTbl );
-//			List <String> keys = new ArrayList <String> ();
-//		    keys.add(args[0]);
-//		    
-//			//dataMap.put("baritems", DemoMain.getJsonData(path,args[0],args[1]));
-//			Map<String, Object> submap_data = new HashMap<String,Object>();
-//			submap_data.put("bgData", dataMap);
-//			submap_data.put("actvScrId", actvScrId);
-//			responseBean.setPayload(submap_data);
-//			responseBean.setActnCode(IDALiteral.UIA_BG);
+ 
 			return algoStr;
 		} catch (NumberFormatException e) {
 			// TODO Auto-generated catch block
