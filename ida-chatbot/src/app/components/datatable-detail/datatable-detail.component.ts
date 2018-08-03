c64974cc57694c0e831c813d5997ffea2c8180a8
nikit91@gmail.com <nikit91@gmail.com>
2018-08-03 15:48:59 +0200

    Expand and Collapse logic added

--- ida-chatbot/src/app/components/datatable-detail/datatable-detail.component.ts
+++ ida-chatbot/src/app/components/datatable-detail/datatable-detail.component.ts
@@ -2,6 +2,7 @@ import {Component, Input, OnInit, ViewChild} from '@angular/core';
 import {DatasetMetadata} from '../../models/dataset-metadata';
 import {DatafileMetadata} from '../../models/datafile-metadata';
 import {IdaEventService} from '../../service/event/ida-event.service';
+import {MatAccordion} from '@angular/material';
 
 
 @Component({
@@ -12,6 +13,10 @@ import {IdaEventService} from '../../service/event/ida-event.service';
 export class DatatableDetailComponent implements OnInit {
   @Input('dsdetail')
   public dsdetail: DatasetMetadata;
+  @ViewChild('tableAcc')
+  public tableAcc: MatAccordion;
+  @ViewChild('colAcc')
+  public colAcc: MatAccordion;
   public displayedColumns: string[] = ['displayName', 'fileName', 'rowCount', 'colCount'];
   public expandedElement: DatafileMetadata;
 
