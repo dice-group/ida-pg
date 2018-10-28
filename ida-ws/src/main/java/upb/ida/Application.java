2d16d84bd1a527993875b46ab66f0d0c0f643891
Clemens Damke <admin@cortys.de>
2018-10-28 18:22:53 +0100

    use XML based bean configuration

--- ida-ws/src/main/java/upb/ida/Application.java
+++ ida-ws/src/main/java/upb/ida/Application.java
@@ -1,10 +1,15 @@
 package upb.ida;
 
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
+@ImportResource({
+	"classpath:config/bean-config.xml",
+	"classpath:config/startup-config.xml"
+})
 public class Application extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
