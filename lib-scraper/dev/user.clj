611ecb2e4035bdc3a521f9f982e21f725b218521
Clemens Damke <admin@cortys.de>
2018-11-04 23:19:14 +0100

    scraping starting point

---
+++ lib-scraper/dev/user.clj
@@ -0,0 +1,18 @@
+(ns user
+  "REPL startup namespace that contains various helper methods to interact with the application."
+  (:require [clojure.tools.namespace.repl :as ctnr]
+            [clojure.stacktrace :refer :all]
+            [proto-repl.saved-values]))
+
+(defn start
+  []
+  (require '[lib-scraper.core :refer :all])
+  (println "REPL started."))
+
+(defn refresh
+  []
+  (ctnr/refresh :after 'user/start))
+
+(defn refresh-all
+  []
+  (ctnr/refresh-all :after 'user/start))
