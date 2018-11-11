29913d34fb7d910b5a553ed0f752c7bb4fcbce30
Nilanjan <nilanjandasmsc@gmail.com>
2018-11-11 15:42:54 +0100

    Testing for clustering algo names

--- ida-ws/src/test/java/upb/ida/test/DataDumpTest.java
+++ ida-ws/src/test/java/upb/ida/test/DataDumpTest.java
@@ -1,5 +1,10 @@
 package upb.ida.test;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +13,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import upb.ida.Application;
+import upb.ida.bean.cluster.ClusterAlgoDesc;
 import upb.ida.util.DataDumpUtil;
 @RunWith(SpringRunner.class)
 @WebMvcTest
@@ -16,9 +22,23 @@ public class DataDumpTest {
 	@Autowired
 	DataDumpUtil dataDumpUtil;
 	
+	
 	@Test
-	public void strLenCompTest() {
-		System.out.println(dataDumpUtil.getClusteringAlgoNames());
+	public void  getClusteringAlgoNamesTest() {
+		List<String> algonames = dataDumpUtil.getClusteringAlgoNames();
+		List<String> resList = new ArrayList<>();
+		resList.add("AffinityPropagation_TEST");
+		resList.add("KMeans_TEST");
+		assertEquals(algonames, resList);
+		assertEquals(algonames.size(),2);
+		
+		//System.out.println(dataDumpUtil.getClusteringAlgoNames());
 	}
 
+	@Test
+	public void Âlgoparams() {
+		//System.out.println(dataDumpUtil.getClusterAlgoParams("KMeans_TEST"));
+	}
+	
+	
 }
