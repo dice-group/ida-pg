1805ca92e2e8539bcac253d061d34e32c9e42f95
AsjadLFC <asohail2@mail.uni-paderborn.de>
2018-11-18 23:33:29 +0100

    artist name query check and JSON format output on controller

--- ida-udf-ws/src/main/java/upb/ida/controllers/sparqlQueryController.java
+++ ida-udf-ws/src/main/java/upb/ida/controllers/sparqlQueryController.java
@@ -1,10 +1,6 @@
 package upb.ida.controllers;
 
-import org.apache.jena.atlas.json.JsonArray;
-import org.apache.jena.atlas.json.JsonObject;
-import org.apache.jena.base.Sys;
 import org.apache.jena.query.*;
-import org.apache.jena.rdf.model.Literal;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.util.FileManager;
 
@@ -13,14 +9,16 @@ import org.springframework.web.bind.annotation.*;
 import java.io.ByteArrayOutputStream;
 
 @RestController
+@RequestMapping("/sparkle")
 public class sparqlQueryController {
-    @GetMapping("/sparkle")
-    public String sparqlTest(){
+
+    @RequestMapping(value = "/artist")
+    public String getbyArtist(@RequestParam("artist") String artist){
         Model model = FileManager.get().loadModel("/home/programmercore/Documents/PG/ida/ida-udf-ws/docs/sample1.rdf");
         String queryString ="PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>" +
                 "PREFIX cd: <http://www.recshop.fake/cd#>" +
                 "SELECT ?person WHERE {" +
-                "?person cd:artist \"Bob Dylan\"" +
+                "?person cd:artist \"" + artist + "\"" +
                 "}";
         Query query = QueryFactory.create(queryString);
         QueryExecution qexec = QueryExecutionFactory.create(query, model);
@@ -29,21 +27,11 @@ public class sparqlQueryController {
             ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
             ResultSetFormatter.outputAsJSON(outputStream, results);
             String json = new String(outputStream.toByteArray());
-            System.out.println(json);
-//            System.out.println(results.hasNext());
-//            while (results.hasNext()){
-//                System.out.println(results.next());
-//
-//            }
-//            return ("Done:" + results);
-//            while(results.hasNext()){
-//                QuerySolution soln = results.nextSolution();
-//                Literal name = soln.getLiteral("x");
-//                return ("Done: " + name);
-//            }
+            return (json);
         } finally {
             qexec.close();
         }
-        return "Done";
     }
+
+
 }
