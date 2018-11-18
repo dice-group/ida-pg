0df6bae0b4347ce1052b5a018bccb66fb2f0deb3
AsjadLFC <asohail2@mail.uni-paderborn.de>
2018-11-18 23:59:46 +0100

    reads all the query, removed the unnecessary things

--- ida-udf-ws/src/main/java/upb/ida/controllers/sparqlQueryController.java
+++ ida-udf-ws/src/main/java/upb/ida/controllers/sparqlQueryController.java
@@ -9,29 +9,22 @@ import org.springframework.web.bind.annotation.*;
 import java.io.ByteArrayOutputStream;
 
 @RestController
-@RequestMapping("/sparkle")
 public class sparqlQueryController {
 
-    @RequestMapping(value = "/artist")
-    public String getbyArtist(@RequestParam("artist") String artist){
+    @RequestMapping(value = "/queryParam")
+    public String getbyArtist(@RequestParam("Param") String queryParam){
         Model model = FileManager.get().loadModel("/home/programmercore/Documents/PG/ida/ida-udf-ws/docs/sample1.rdf");
         String queryString ="PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>" +
-                "PREFIX cd: <http://www.recshop.fake/cd#>" +
-                "SELECT ?person WHERE {" +
-                "?person cd:artist \"" + artist + "\"" +
-                "}";
+                "PREFIX cd: <http://www.recshop.fake/cd#> " + queryParam;
+
         Query query = QueryFactory.create(queryString);
         QueryExecution qexec = QueryExecutionFactory.create(query, model);
-        try{
-            ResultSet results = qexec.execSelect();
-            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-            ResultSetFormatter.outputAsJSON(outputStream, results);
-            String json = new String(outputStream.toByteArray());
-            return (json);
-        } finally {
-            qexec.close();
-        }
+        ResultSet results = qexec.execSelect();
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        ResultSetFormatter.outputAsJSON(outputStream, results);
+
+        String json = new String(outputStream.toByteArray());
+        return (json);
     }
-
-
 }
