c86866a18f27f7b351d826c89880f611a04960c5
AsjadLFC <asohail2@mail.uni-paderborn.de>
2018-11-18 18:05:29 +0100

    query work

--- ida-udf-ws/src/main/java/upb/ida/controllers/sparqlQueryController.java
+++ ida-udf-ws/src/main/java/upb/ida/controllers/sparqlQueryController.java
@@ -1,5 +1,6 @@
 package upb.ida.controllers;
 
+import org.apache.jena.base.Sys;
 import org.apache.jena.query.*;
 import org.apache.jena.rdf.model.Literal;
 import org.apache.jena.rdf.model.Model;
@@ -11,23 +12,30 @@ import org.springframework.web.bind.annotation.*;
 public class sparqlQueryController {
     @GetMapping("/sparkle")
     public String sparqlTest(){
-        Model model = FileManager.get().loadModel("/home/programmercore/Documents/PG/ida/ida-udf-ws/docs/sample_person.ttl");
-        String queryString ="SELECT ?title{" +
-                "<http://dbpedia.org/resource/Karel_Matěj_Čapek-Chod> <http://dbpedia.org/ontology/birthPlace> ?title" +
+        Model model = FileManager.get().loadModel("/home/programmercore/Documents/PG/ida/ida-udf-ws/docs/sample1.rdf");
+        String queryString ="PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>" +
+                "PREFIX cd: <http://www.recshop.fake/cd#>" +
+                "SELECT * WHERE {" +
+                "?person cd:company ?artist" +
                 "}";
         Query query = QueryFactory.create(queryString);
         QueryExecution qexec = QueryExecutionFactory.create(query, model);
 
         try{
             ResultSet results = qexec.execSelect();
-            while(results.hasNext()){
-                QuerySolution soln = results.nextSolution();
-                Literal name = soln.getLiteral("x");
-                return ("Done: " + name);
+            System.out.println(results.hasNext());
+            while (results.hasNext()){
+                System.out.println(results.next());
             }
+//            return ("Done:" + results);
+//            while(results.hasNext()){
+//                QuerySolution soln = results.nextSolution();
+//                Literal name = soln.getLiteral("x");
+//                return ("Done: " + name);
+//            }
         } finally {
             qexec.close();
         }
-        return null;
+        return "Done";
     }
 }
