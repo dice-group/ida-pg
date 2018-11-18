ff02930ff96e74f82a5697151c04eec4b60d7083
AsjadLFC <asohail2@mail.uni-paderborn.de>
2018-11-18 21:42:51 +0100

    output in JSON format

--- ida-udf-ws/src/main/java/upb/ida/controllers/sparqlQueryController.java
+++ ida-udf-ws/src/main/java/upb/ida/controllers/sparqlQueryController.java
@@ -1,5 +1,7 @@
 package upb.ida.controllers;
 
+import org.apache.jena.atlas.json.JsonArray;
+import org.apache.jena.atlas.json.JsonObject;
 import org.apache.jena.base.Sys;
 import org.apache.jena.query.*;
 import org.apache.jena.rdf.model.Literal;
@@ -8,6 +10,8 @@ import org.apache.jena.util.FileManager;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.io.ByteArrayOutputStream;
+
 @RestController
 public class sparqlQueryController {
     @GetMapping("/sparkle")
@@ -15,18 +19,22 @@ public class sparqlQueryController {
         Model model = FileManager.get().loadModel("/home/programmercore/Documents/PG/ida/ida-udf-ws/docs/sample1.rdf");
         String queryString ="PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>" +
                 "PREFIX cd: <http://www.recshop.fake/cd#>" +
-                "SELECT * WHERE {" +
-                "?person cd:company ?artist" +
+                "SELECT ?person WHERE {" +
+                "?person cd:artist \"Bob Dylan\"" +
                 "}";
         Query query = QueryFactory.create(queryString);
         QueryExecution qexec = QueryExecutionFactory.create(query, model);
-
         try{
             ResultSet results = qexec.execSelect();
-            System.out.println(results.hasNext());
-            while (results.hasNext()){
-                System.out.println(results.next());
-            }
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            ResultSetFormatter.outputAsJSON(outputStream, results);
+            String json = new String(outputStream.toByteArray());
+            System.out.println(json);
+//            System.out.println(results.hasNext());
+//            while (results.hasNext()){
+//                System.out.println(results.next());
+//
+//            }
 //            return ("Done:" + results);
 //            while(results.hasNext()){
 //                QuerySolution soln = results.nextSolution();
