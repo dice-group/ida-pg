43b941d27b6c50e45437671c0a1ef3948539f1f5
Deepak Garg <erdkgarg08@gmail.com>
2018-11-18 23:07:20 +0100

    Commit changes by Deepak and Ali
    New features added

--- speech2text/mzsound.py
+++ speech2text/mzsound.py
@@ -1,13 +1,18 @@
 import speech_recognition as sr
+import webbrowser
+while(1):
 
-r = sr.Recognizer()
-with sr.Microphone() as source:
-    print("Speak:")
-    audio = r.listen(source)
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Speak:")
+        audio = r.listen(source)
 
-try:
-    print("You said " + r.recognize_google(audio))
-except sr.UnknownValueError:
-    print("Could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results; {0}".format(e))
+    try:
+        print("You said " + r.recognize_google(audio))
+        S = r.recognize_google(audio)
+        if S=="google":
+            webbrowser.open('http://google.com')
+    except sr.UnknownValueError:
+        print("Could not understand audio")
+    except sr.RequestError as e:
+        print("Could not request results; {0}".format(e))
