a1245e94e849065811573cc0b0537d7534cd10b4
Maqbool ur Rahim khan <maqboolurrahim@gmail.com>
2018-11-20 16:28:23 +0100

    feat(rdf sparql server): XML file to RDF done

--- ida-udf-ws/src/main/java/upb/ida/Application.java
+++ ida-udf-ws/src/main/java/upb/ida/Application.java
@@ -3,10 +3,13 @@ package upb.ida;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.File;
+
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
+        new File(System.getProperty("user.dir") + "/uploads").mkdir();
         SpringApplication.run(Application.class, args);
     }
 }
--- ida-udf-ws/src/main/java/upb/ida/controllers/XMLAdapterController.java
+++ ida-udf-ws/src/main/java/upb/ida/controllers/XMLAdapterController.java
@@ -2,21 +2,47 @@ package upb.ida.controllers;
 
 import no.acando.xmltordf.Builder;
 import org.apache.jena.query.Dataset;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.xml.sax.SAXException;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @RestController
+@RequestMapping("/adapter/xml")
 public class XMLAdapterController {
 
-    @PostMapping("/adapter/xml")
+    @PostMapping("/")
     public String convert(@RequestBody String text) throws IOException, SAXException, ParserConfigurationException {
-        InputStream inputStream = new ByteArrayInputStream(text.getBytes(StandardCharsets.UTF_8));
+        // (TODO): Request's Validation
+        byte bytes[] = text.getBytes(StandardCharsets.UTF_8);
+        return new String(xmlToRDF(bytes));
+    }
+
+    @PostMapping("/file")
+    public String singleFileUpload(@RequestParam(value="file") MultipartFile file) {
+        // (TODO): Request's Validation
+
+        try {
+            // Reading file's content in bytes
+            byte[] bytes = file.getBytes();
+            Path path = Paths.get(System.getProperty("user.dir") + "/uploads/", file.getOriginalFilename() + ".ttl");
+            Files.write(path, xmlToRDF(bytes));
+        } catch (IOException | SAXException | ParserConfigurationException e) {
+            e.printStackTrace();
+        }
+
+        // (TODO) We need proper Response bean
+        return "done";
+    }
+
+    private byte[] xmlToRDF(byte[] bytes) throws IOException, SAXException, ParserConfigurationException {
+        InputStream inputStream = new ByteArrayInputStream(bytes);
 
         BufferedInputStream in = new BufferedInputStream(inputStream);
         Dataset dataset = Builder.getAdvancedBuilderJena().build().convertToDataset(in);
@@ -24,6 +50,6 @@ public class XMLAdapterController {
         //  converting it into turtle format
         dataset.getDefaultModel().write(stream, "ttl");
 
-        return new String(stream.toByteArray());
+        return stream.toByteArray();
     }
 }
