f282d556abb16b5099a79688716e7a3ed2d56aac
bhatmanjokes <adityabhat.h@gmail.com>
2018-11-26 08:48:11 +0100

    Adding code coverage handling to Travis yml and keeping the Angular tests on hold

---
+++ ida-chatbot/src/app/components/intro/intro.component.mock.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-intro'
+})
+export class MockIntroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
