7786118590be5e8a63c5460ce9d3cfb941da9ebb
Clemens Damke <admin@cortys.de>
2018-11-29 23:03:05 +0100

    split skl specifics from generic crawler

--- lib-scraper/src/lib_scraper/core.clj
+++ lib-scraper/src/lib_scraper/core.clj
@@ -1,19 +1,31 @@
 (ns lib-scraper.core
-  (:require [lib-scraper.crawler.core :refer [crawl]]
+  (:require [hickory.core :refer [parse]]
+            [hickory.select :as s]
+            [lib-scraper.crawler.core :refer [crawl]]
             [lib-scraper.helpers.predicate :refer [p-and p-or p-expire p-log]]
             [lib-scraper.match.links :as l])
   (:import (edu.uci.ics.crawler4j.crawler Page)
-           (edu.uci.ics.crawler4j.url WebURL)))
+           (edu.uci.ics.crawler4j.url WebURL)
+           (edu.uci.ics.crawler4j.parser HtmlParseData)))
+
+(defn crawl-docs
+  [seed should-visit visit]
+  (crawl {:seed seed
+          :should-visit (p-or (l/match-url seed)
+                              (p-and (p-expire 1)
+                                     should-visit
+                                     (p-log (fn [_, ^Page page, ^WebURL url]
+                                              (str "traversed: " url)))))
+          :visit (fn [_, ^Page page]
+                   (let [parse-data (.getParseData page)
+                         document (when (instance? HtmlParseData parse-data)
+                                    (parse (.getHtml ^HtmlParseData parse-data)))]
+                     (when document
+                       (visit document))))
+          :max-depth 1}))
 
 (defn crawl-skl
   []
-  (let [seed "http://scikit-learn.org/0.20/modules/classes.html"]
-    (crawl {:seed seed
-            :should-visit (p-and (p-expire 3)
-                                 (p-or (l/match-url seed)
-                                       (l/require-classes #{"reference" "internal"}))
-                                 (p-log (fn [_, ^Page page, ^WebURL url]
-                                          (str url (into {} (.getAttributes url))))))
-            :visit (fn [_, ^Page page])
-                     ;(println (.getHtml (.getParseData page))))
-            :max-depth 1})))
+  (crawl-docs "https://scikit-learn.org/0.20/modules/classes.html"
+              (l/require-classes #{"reference" "internal"})
+              (comp println type)))
