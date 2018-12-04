dff52eb048222c29e8a92b49a5568f52828aca83
Ayaz Maqbool <ayazmaqbooluet@gmail.com>
2018-12-05 00:26:20 +0100

    resolved the issue of build failure on travis- changed the aspect logger from component to bean

--- ida-ws/src/main/java/upb/ida/logging/LoggerProvider.java
+++ ida-ws/src/main/java/upb/ida/logging/LoggerProvider.java
@@ -1,5 +1,7 @@
 package upb.ida.logging;
 
+import javax.annotation.PostConstruct;
+
 /**
  * Class to expose logger provider methods for Aspect Logger in IDA
  * 
@@ -21,53 +23,53 @@ import upb.ida.util.FileUtil;
 
 @Component
 public class LoggerProvider {
-	
+
 	@Autowired
 	private FileUtil fileUtil;
-	
-	
+
+	@PostConstruct
+	public void setLogProps() {
+		String log4jConfigFile = fileUtil.fetchSysFilePath(IDALiteral.ASPECTLOGGER_PROP_FILEPATH);
+		PropertyConfigurator.configure(log4jConfigFile);
+	}
+
 	/**
-	 * Method to get the logger instance for RequestResponseLogger from a log4j.properties file
+	 * Method to get the logger instance for RequestResponseLogger from a
+	 * log4j.properties file
 	 * 
 	 * @return - RequestResponseLogger
 	 */
 	@Bean
 	@Scope(value = "singleton", proxyMode = ScopedProxyMode.TARGET_CLASS)
 	@Qualifier("requestResponseLogger")
-	public Logger getRequestResponseLogger() 
-	{
-		String log4jConfigFile = fileUtil.fetchSysFilePath(IDALiteral.ASPECTLOGGER_PROP_FILEPATH);
-		PropertyConfigurator.configure(log4jConfigFile);
+	public Logger getRequestResponseLogger() {
+
 		return Logger.getLogger("RequestResponseLogger");
 	}
-	
+
 	/**
-	 * Method to get the logger instance for ExceptionLogger from a log4j.properties file
+	 * Method to get the logger instance for ExceptionLogger from a log4j.properties
+	 * file
 	 * 
 	 * @return - ExceptionLogger
 	 */
 	@Bean
 	@Scope(value = "singleton", proxyMode = ScopedProxyMode.TARGET_CLASS)
 	@Qualifier("exceptionLogger")
-	public Logger getExceptionLogger() 
-	{
-		String log4jConfigFile = fileUtil.fetchSysFilePath("log4j.properties");
-		PropertyConfigurator.configure(log4jConfigFile);
+	public Logger getExceptionLogger() {
 		return Logger.getLogger("ExceptionLogger");
 	}
-	
+
 	/**
-	 * Method to get the logger instance for DatasetResponseLogger from a log4j.properties file
+	 * Method to get the logger instance for DatasetResponseLogger from a
+	 * log4j.properties file
 	 * 
 	 * @return - DatasetResponseLogger
 	 */
 	@Bean
 	@Scope(value = "singleton", proxyMode = ScopedProxyMode.TARGET_CLASS)
 	@Qualifier("datasetResponseLogger")
-	public Logger getDatasetReponseLogger() 
-	{
-		String log4jConfigFile = fileUtil.fetchSysFilePath("log4j.properties");
-		PropertyConfigurator.configure(log4jConfigFile);
+	public Logger getDatasetReponseLogger() {
 		return Logger.getLogger("DatasetResponseLogger");
 	}
 }
