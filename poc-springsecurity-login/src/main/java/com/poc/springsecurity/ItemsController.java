88f55d3465b65544659976223c3278ca9e686b0a
Ayaz Maqbool <ayazmaqbooluet@gmail.com>
2018-12-06 20:33:39 +0100

    added controllers to poc spring security login

---
+++ poc-springsecurity-login/src/main/java/com/poc/springsecurity/ItemsController.java
@@ -0,0 +1,23 @@
+package com.poc.springsecurity;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ItemsController {
+
+    private static final String template = "The item is, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/getItem")
+    public Item information(@RequestParam(value="item", defaultValue="Milk") String name) {
+        return new Item(counter.incrementAndGet(), String.format(template, name));
+    }
+
+    @RequestMapping("/coupon")
+    public String greeting(@RequestParam(value="item") String name) {
+        return "The coupon code is: XASFDA";
+    }
+}
