07e7bc1547366874c74e7eb822480402dd577829
AsjadLFC <asjad.sohail@outlook.com>
2018-12-07 16:46:36 +0100

    alternate approach for turtle datasets

--- ida-udf-ws/src/main/java/upb/ida/controllers/SparqlQueryController.java
+++ ida-udf-ws/src/main/java/upb/ida/controllers/SparqlQueryController.java
@@ -2,29 +2,35 @@ package upb.ida.controllers;
 
 import org.apache.jena.query.*;
 import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdfconnection.RDFConnection;
+import org.apache.jena.rdfconnection.RDFConnectionFactory;
 import org.apache.jena.util.FileManager;
 
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 @RestController
 public class SparqlQueryController {
 
-    @RequestMapping(value = "/queryParam")
-    public String getbyArtist(@RequestParam("Param") String queryParam){
-        Model model = FileManager.get().loadModel("/home/programmercore/Documents/PG/ida/ida-udf-ws/docs/sample1.rdf");
-        String queryString ="PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>" +
-                "PREFIX cd: <http://www.recshop.fake/cd#> " + queryParam;
+//    protected static String fusekiQueryPath = "http://localhost:3330/ds/query";
+//    String outputStatement = null;
 
+    @RequestMapping(value = "/queryParam")
+    public String getbyArtist(@RequestParam("Param") String queryParam, @RequestParam("file") String datasetName){
+        Model model = FileManager.get().loadModel(System.getProperty("user.dir") + "/uploads/" + datasetName + ".ttl");
+        String queryString = queryParam;
         Query query = QueryFactory.create(queryString);
         QueryExecution qexec = QueryExecutionFactory.create(query, model);
         ResultSet results = qexec.execSelect();
-
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         ResultSetFormatter.outputAsJSON(outputStream, results);
-
         String json = new String(outputStream.toByteArray());
-        return (json);
+        return json;
     }
 }
