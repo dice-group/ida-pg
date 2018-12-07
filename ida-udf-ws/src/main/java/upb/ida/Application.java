6b856082ac0a5e37a2e4c9b56416f2c615e1efe4
Maqbool ur Rahim khan <maqboolurrahim@gmail.com>
2018-12-07 15:58:46 +0100

    commiting code for review

--- ida-udf-ws/src/main/java/upb/ida/Application.java
+++ ida-udf-ws/src/main/java/upb/ida/Application.java
@@ -1,22 +1,10 @@
 package upb.ida;
 
-import org.apache.jena.fuseki.Fuseki;
 import org.apache.jena.fuseki.main.FusekiServer;
 import org.apache.jena.query.Dataset;
 import org.apache.jena.query.DatasetFactory;
-import org.apache.jena.rdfconnection.RDFConnection;
-import org.apache.jena.rdfconnection.RDFConnectionFactory;
-import org.apache.jena.rdfconnection.RDFConnectionFuseki;
-import org.apache.jena.rdfconnection.RDFConnectionRemoteBuilder;
-import org.apache.jena.riot.RDFDataMgr;
-import org.apache.jena.sparql.core.DatasetGraph;
-import org.apache.jena.sparql.core.DatasetGraphFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import javax.xml.crypto.Data;
-import java.io.File;
-import java.io.IOException;
 
 @SpringBootApplication
 public class Application {
--- ida-udf-ws/src/main/java/upb/ida/controllers/SparqlQueryController.java
+++ ida-udf-ws/src/main/java/upb/ida/controllers/SparqlQueryController.java
@@ -19,33 +19,12 @@ import java.io.IOException;
 @RestController
 public class SparqlQueryController {
 
-    protected static String fusekiQueryPath = "http://localhost:3330/ds/query";
-    String outputStatement = null;
-
     @RequestMapping(value = "/")
     public String getbyArtist(@RequestParam("Param") String queryParam){
-//        FileManager.get().loadModel("/home/programmercore/Documents/PG/ida/ida-udf-ws/uploads/sampleresult.ttl");
         queryParam = "SELECT distinct ?c WHERE { ?a test:City ?c . }";
-        String queryString = "PREFIX test:  <http://example.com#>" + queryParam;
-//        QueryExecution qexec = QueryExecutionFactory.sparqlService(fusekiQueryPath, queryString);
-//        ResultSet results = qexec.execSelect();
-
-//        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-//        String outputStatement = null;
-
-//        ResultSetFormatter.outputAsJSON(outputStream, results);
-//        String json = new String(outputStream.toByteArray());
-//        return json;
-        Model model = ModelFactory.createDefaultModel();
-        try{
-            model.read(new FileInputStream("/home/programmercore/Documents/PG/ida/ida-udf-ws/uploads/sampleresult.ttl"),null, "TURTLE");
-        }catch(Exception e){
-            e.printStackTrace();
-        }
-
-//        Dataset dataset = DatasetFactory.createTxnMem();
+        String queryString = queryParam;
         RDFConnection conn = RDFConnectionFactory.connect("http://localhost:3330/ds/query");
-        conn.load(model);
+        conn.load(System.getProperty("user.dir") + "/uploads/dataset1.ttl");
         QueryExecution queryExecution = conn.query(queryString);
         ResultSet results = queryExecution.execSelect();
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
@@ -53,46 +32,5 @@ public class SparqlQueryController {
         String json = new String(outputStream.toByteArray());
         conn.end();
         return json;
-//        String json = new String(outputStream.toByteArray());
-//        return json;
-//        if(output.toLowerCase() == "json"){
-//
-//            ResultSetFormatter.outputAsJSON(outputStream, results);
-//            String json = new String(outputStream.toByteArray());
-//
-//            outputStatement = json;
-//
-//        } else if(output.toLowerCase() == "csv"){
-//
-//            ResultSetFormatter.outputAsCSV(outputStream, results);
-//            String json = new String(outputStream.toByteArray());
-//
-//            outputStatement = json;
-//
-//        } else if(output.toLowerCase() == "xml"){
-//
-//            ResultSetFormatter.outputAsXML(outputStream, results);
-//            String json = new String(outputStream.toByteArray());
-//
-//            outputStatement = json;
-//
-//        } else {
-//            outputStatement = "Format not Supported!";
-//        }
-//        return outputStatement;
-//        return null;
     }
-
-//    @PostMapping("/fileUpload")
-//    public static void uploadFile(@RequestParam(value = "ttlfile") File file) throws IOException {
-//        Model model = ModelFactory.createDefaultModel();
-//        try (FileInputStream in = new FileInputStream(file)) {
-//            model.read(in, null, "RDF/XML");
-//        }
-//
-//        // upload the resulting model
-//        DatasetAccessor accessor = DatasetAccessorFactory
-//                .createHTTP(serviceURI);
-//        accessor.putModel(m);
-//    }
 }
