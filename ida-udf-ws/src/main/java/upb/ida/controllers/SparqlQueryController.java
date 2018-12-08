0fce45cab9b3f459b481579cc70601dd8c09ad37
Maqbool ur Rahim khan <maqboolurrahim@gmail.com>
2018-12-08 01:03:10 +0100

    code refactoring

--- ida-udf-ws/src/main/java/upb/ida/controllers/SparqlQueryController.java
+++ ida-udf-ws/src/main/java/upb/ida/controllers/SparqlQueryController.java
@@ -2,29 +2,19 @@ package upb.ida.controllers;
 
 import org.apache.jena.query.*;
 import org.apache.jena.rdf.model.Model;
-import org.apache.jena.rdf.model.ModelFactory;
-import org.apache.jena.rdfconnection.RDFConnection;
-import org.apache.jena.rdfconnection.RDFConnectionFactory;
 import org.apache.jena.util.FileManager;
 
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
 
 @RestController
 public class SparqlQueryController {
 
-//    protected static String fusekiQueryPath = "http://localhost:3330/ds/query";
-//    String outputStatement = null;
-
-    @RequestMapping(value = "/queryParam")
-    public String getbyArtist(@RequestParam("Param") String queryParam, @RequestParam("file") String datasetName){
+    @GetMapping(value = "/sparql")
+    public String getbyArtist(@RequestParam("query") String queryString, @RequestParam("datasetName") String datasetName) {
+        // TODO: Error handling e.g. if dataset name is invalid
         Model model = FileManager.get().loadModel(System.getProperty("user.dir") + "/uploads/" + datasetName + ".ttl");
-        String queryString = queryParam;
         Query query = QueryFactory.create(queryString);
         QueryExecution qexec = QueryExecutionFactory.create(query, model);
         ResultSet results = qexec.execSelect();
