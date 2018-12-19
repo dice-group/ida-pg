4ce7ddedacd3855ec59ec934f8d5e7ea6d9c34ab
Maqbool ur Rahim khan <maqboolurrahim@gmail.com>
2018-12-19 01:46:57 +0100

    Now user can iterate over messages just like in Terminal

--- ida-chatbot/src/app/components/message-form/message-form.component.ts
+++ ida-chatbot/src/app/components/message-form/message-form.component.ts
@@ -9,9 +9,12 @@ import {RestService} from '../../service/rest/rest.service';
 })
 export class MessageFormComponent implements OnInit {
   @Output() msgemitter = new EventEmitter<Message>();
-  constructor(private restservice: RestService) { }
-  public showBar = false;
   @ViewChild('chatmsg') chatmsg: ElementRef;
+
+  public showBar = false;
+  msgs_history = [];
+  msgs_tracker = 0;
+  constructor(private restservice: RestService) { }
 
   ngOnInit() {
     this.restservice.requestEvnt.subscribe(val => { this.toggleProgressBar(val); });
@@ -26,11 +29,19 @@ export class MessageFormComponent implements OnInit {
       const curTime = new Date();
       const message = new Message(msg, 'User', 'user', curTime);
       this.chatmsg.nativeElement.value = '';
+      this.msgs_history.push(msg);
       this.msgemitter.emit(message);
+      this.msgs_tracker = this.msgs_history.length;
     }
   }
 
-  keyDownFunction() {
-    this.sendMessage();
+  iterateMsgs(direction) {
+    if (this.msgs_history.length) {
+      if (direction === 'up' && this.msgs_tracker > 0) {
+        this.chatmsg.nativeElement.value = this.msgs_history[--this.msgs_tracker];
+      } else if (direction === 'down' && this.msgs_tracker < this.msgs_history.length - 1) {
+        this.chatmsg.nativeElement.value = this.msgs_history[++this.msgs_tracker];
+      }
+    }
   }
 }
