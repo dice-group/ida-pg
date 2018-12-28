220314fce6a3b0868f99a78050ff20dc5ad08cb8
Clemens Damke <admin@cortys.de>
2018-12-28 19:09:33 +0100

    added support for standalone functions and basic datatypes

--- lib-scraper/src/lib_scraper/model/concepts/core.clj
+++ lib-scraper/src/lib_scraper/model/concepts/core.clj
@@ -3,10 +3,12 @@
             [lib-scraper.model.concepts.package :as package]
             [lib-scraper.model.concepts.class :as class]
             [lib-scraper.model.concepts.function :as function]
-            [lib-scraper.model.concepts.parameter :as parameter]))
+            [lib-scraper.model.concepts.parameter :as parameter]
+            [lib-scraper.model.concepts.datatype :as datatype]))
 
 (def concepts (merge common/spec
                      package/spec
                      class/spec
                      function/spec
-                     parameter/spec))
+                     parameter/spec
+                     datatype/spec))
