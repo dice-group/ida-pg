a17d8c64389372f0b88cb0abcad76765862df0d0
Clemens Damke <admin@cortys.de>
2018-12-28 22:45:25 +0100

    scrape parameter optionality data
    + minor schema and traversal fixes

--- lib-scraper/src/lib_scraper/model/concepts/parameter.clj
+++ lib-scraper/src/lib_scraper/model/concepts/parameter.clj
@@ -3,4 +3,6 @@
 (def spec {::name {:db/type :db.type/string
                    :db/doc "Name of the parameter."}
            ::position {:db/type :db.type/long
-                       :db/doc "Position of the parameter."}})
+                       :db/doc "Position of the parameter."}
+           ::optional {:db/type :db.type/boolean
+                       :db/doc "Denotes whether this parameter is optional."}})
