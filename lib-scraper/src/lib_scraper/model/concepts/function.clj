4c16b24ab4f7260603b699fa5faf4cc46ddf67aa
Clemens Damke <admin@cortys.de>
2018-12-31 20:14:42 +0100

    bugfix: conditional key specs did not work

--- lib-scraper/src/lib_scraper/model/concepts/function.clj
+++ lib-scraper/src/lib_scraper/model/concepts/function.clj
@@ -1,6 +1,8 @@
 (ns lib-scraper.model.concepts.function
   (:require [clojure.spec.alpha :as s]
-            [lib-scraper.helpers.spec :as hs]))
+            [lib-scraper.helpers.spec :as hs]
+            [lib-scraper.model.concepts.package :as package]
+            [lib-scraper.model.concepts.class :as class]))
 
 (def concept {::name {:db/type :db.type/string
                       :db/doc "Name of the function."}
@@ -9,4 +11,6 @@
                        :db/doc "A parameter of the function."}})
 
 (s/def ::name string?)
-(s/def ::concept (hs/entity-keys :req [::name]))
+(s/def ::concept (hs/entity-keys :req [::name (or ::class/_method
+                                                  ::class/_constructor
+                                                  ::package/_member)]))
