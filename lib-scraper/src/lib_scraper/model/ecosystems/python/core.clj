0ae3b0539bfa7c19b224cc44f5fb876f115e0bf0
Clemens Damke <admin@cortys.de>
2019-01-16 20:22:16 +0100

    improved python language model
    it now knows that a class in python has exactly one constructor. this fact allows for automatic unification of found constructor information during scraping.

--- lib-scraper/src/lib_scraper/model/ecosystems/python/core.clj
+++ lib-scraper/src/lib_scraper/model/ecosystems/python/core.clj
@@ -2,8 +2,10 @@
   (:require [lib-scraper.model.syntax :refer [defecosystem]]
             [lib-scraper.model.paradigms.oo.core :refer [oo]]
             [lib-scraper.model.paradigms.functional.core :refer [functional]]
-            [lib-scraper.model.ecosystems.python.class :refer [class]])
+            [lib-scraper.model.ecosystems.python.class :refer [class]]
+            [lib-scraper.model.ecosystems.python.constructor :refer [constructor]])
   (:refer-clojure :exclude [class]))
 
 (defecosystem python [oo functional]
-  :class class)
+  :class class
+  :constructor constructor)
