3eab9f72be5cdd7c44ece672afe8b0e8d5b2f341
Clemens Damke <admin@cortys.de>
2019-01-16 20:43:16 +0100

    improved lib-scraper Java interop API
    does not wrap the Clojure datastructures anymore since those already implement all the common Java interfaces.

--- lib-scraper/src/lib_scraper/interop/scrape.clj
+++ lib-scraper/src/lib_scraper/interop/scrape.clj
@@ -2,7 +2,6 @@
   (:require [lib-scraper.io.core :as io]
             [lib-scraper.io.scrape :as scrape]
             [clojure.edn :as edn])
-  (:import (java.util ArrayList))
   (:gen-class :name lib_scraper.interop.Scrape
               :prefix "scrape-"
               :state scrape
@@ -10,9 +9,11 @@
               :factory load
               :main false
               :constructors {[String] []
-                                   [java.io.File] []}
-              :methods [[query [String] java.util.ArrayList]
-                        [query [String java.lang.Iterable] java.util.ArrayList]
+                             [java.io.File] []}
+              :methods [[query [String] java.util.List]
+                        [query [String java.lang.Iterable] java.util.List]
+                        [querySingle [String] Object]
+                        [querySingle [String java.lang.Iterable] Object]
                         [getName [] String]
                         [getCreationDate [] java.util.Date]
                         [getEcosystemName [] String]
@@ -22,14 +23,18 @@
   [file]
   [[] (scrape/read-scrape file)])
 
+(defn scrape-querySingle
+  ([this, ^String query]
+   (scrape-querySingle this query []))
+  ([this, ^String query, ^java.lang.Iterable inputs]
+   (apply io/query-scrape (.scrape this)
+          (edn/read-string query) inputs)))
+
 (defn scrape-query
   ([this, ^String query]
-   (scrape-query this query []))
-  ([this, ^String query, inputs]
-   (let [result (apply io/query-scrape (.scrape this)
-                       (edn/read-string query) inputs)]
-     (ArrayList. (map (partial into-array Object)
-                      result)))))
+   (vec (scrape-querySingle this query)))
+  ([this, ^String query, ^java.lang.Iterable inputs]
+   (vec (scrape-querySingle this query inputs))))
 
 (defn scrape-getName
   [this]
