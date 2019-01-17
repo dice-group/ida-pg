dd55818d08a111aa8b745911ceb659e91abae455
Ayaz Maqbool <ayazmaqbooluet@gmail.com>
2019-01-17 18:31:46 +0100

    email signup functionality completed with mail.uni-paderbon.de smtp server

--- poc-springsecurity-login/src/main/java/com/poc/smtp/EmailForSignup.java
+++ poc-springsecurity-login/src/main/java/com/poc/smtp/EmailForSignup.java
@@ -1,5 +1,6 @@
 package com.poc.smtp;
 
+import java.io.UnsupportedEncodingException;
 import java.util.Properties;
 
 import javax.mail.Message;
@@ -12,110 +13,74 @@ import javax.activation.*;
 
 public class EmailForSignup {
 
-/*	
-	//Code for testing the simple message sent from localhost
-	public static void main(String [] args) {
-		String to = "itsayazmaqbool@gmail.com";
-		String from = "managerdice@gmail.com";
-		String host = "localhost";
-
-		Properties properties = System.getProperties();
-		properties.setProperty("mail.smtp.host", host);
-
-		Session session = Session.getDefaultInstance(properties);
-		try {
-			MimeMessage message = new MimeMessage(session);
-			message.setFrom(new InternetAddress(from));
-		    message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
-		    message.setSubject("This is the Subject Line!");
-		    message.setText("This is actual message");
-		    Transport.send(message);
-		    System.out.println("Sent message successfully....");
-		} catch (MessagingException mex) {
-			mex.printStackTrace();
-		}
-	}
+	private static String ToUserName;
 	
-*/	
-	//Complete Code using Transport protocol for SMTP Server
-    static final String FROM = "ayazmaqbooluet@gmail.com";
-    static final String FROMNAME = "DICE Manager";
-
-    static final String TO = "itsayazmaqbool@gmail.com";
+	private static final String FROM = "dice-ida@mail.uni-paderborn.de";
+	private static final String FROMNAME = "DICE Manager";
+	private static final String SMTP_USERNAME = "";
+	private static final String SMTP_PASSWORD = "";
+	private static final String CONFIGSET = "ConfigSet";
     
-    //TODO: need to setup the smtp server on uni paderborn server
-    static final String SMTP_USERNAME = "smtp_username";
-    static final String SMTP_PASSWORD = "smtp_password";
+	private static final String HOST = "mail.uni-paderborn.de"; 
+	private static final int PORT = 25;
     
-    static final String CONFIGSET = "ConfigSet";
-    
-    static final String HOST = "localhost"; //"email-smtp.uni-paderborn.com";
-    
-
-    static final int PORT = 587;
-    
-    static final String SUBJECT = "DICE Signup Email";
-    
-    static final String BODY = String.join(
+	private static final String SUBJECT = "DICE Signup Email";
+	private static final String BODY = String.join(
     	    System.getProperty("line.separator"),
-    	    "<h1>DICE SIGNUP CONFIRMATION</h1>",
-    	    "<p>This email was sent for the confimation of the signup</p>"
+    	    "<h1>WELCOME</h1>",
+    	    "<p>Dear User,"
+    	    + "<br><br>"
+    	    + "Your new IDA user has been created. Welcome to the IDA Community!"
+    	    + "<br>"
+    	    + "From now on, please log in to your account using your email address and password."
+    	    + "<br><br>"
+    	    + "Thanks for Registering."
+    	    + "<br>"
+    	    + "DICE TEAM</p>"
     	);
 
-    public static void main(String[] args) throws Exception {
-
-        // Create a Properties object to contain connection configuration information.
+    public static void sendEmail(String userName) throws Exception
+    {
+    	if(userName.isEmpty()) return;
+    	ToUserName = userName;
     	Properties props = System.getProperties();
     	props.put("mail.transport.protocol", "smtp");
+    	props.put("mail.smtp.host", HOST);
     	props.put("mail.smtp.port", PORT); 
     	props.put("mail.smtp.starttls.enable", "true");
     	props.put("mail.smtp.auth", "true");
 
-        // Create a Session object to represent a mail session with the specified properties. 
     	Session session = Session.getDefaultInstance(props);
-
-        // Create a message with the specified information.
-    	SmtpAuthenticator authenticator = new SmtpAuthenticator();
-    	javax.mail.Message msg = new MimeMessage(Session
-    	                    .getInstance(props, authenticator));
     	
-        //MimeMessage msg = new MimeMessage(session);
+        MimeMessage msg = new MimeMessage(session);
         msg.setFrom(new InternetAddress(FROM,FROMNAME));
-        msg.setRecipient(Message.RecipientType.TO, new InternetAddress(TO));
+        msg.setRecipient(Message.RecipientType.TO, new InternetAddress(ToUserName));
         msg.setSubject(SUBJECT);
         msg.setContent(BODY,"text/html");
-        
-        // Add a configuration set header. Comment or delete the 
-        // next line if you are not using a configuration set
         msg.setHeader("X-SES-CONFIGURATION-SET", CONFIGSET);
             
-        // Create a transport.
-        Transport transport = session.getTransport();
-                    
-        // Send the message.
+        Transport transport = session.getTransport();        
         try
-        {
-            System.out.println("Sending...");
-            
-            // Connect to Uni Paderborn Server using the SMTP username and password you specified above.
-            //transport.connect(HOST, SMTP_USERNAME, SMTP_PASSWORD);
+        {          
+            transport.connect(HOST, SMTP_USERNAME, SMTP_PASSWORD);
         	
-            // Send the email.
-            Transport.send(msg);
-            //transport.sendMessage(msg, msg.getAllRecipients());
+            transport.sendMessage(msg, msg.getAllRecipients());
             System.out.println("Email sent!");
         }catch (MessagingException mex) {
             mex.printStackTrace();
         }
-        //catch (Exception ex) {
-        //    System.out.println("The email was not sent.");
-        //    System.out.println("Error message: " + ex.getMessage());
-        //}
+        catch (Exception ex) {
+            System.out.println("The email was not sent.");
+            System.out.println("Error message: " + ex.getMessage());
+        }
         finally
         {
-            // Close and terminate the connection.
             transport.close();
         }
     }
-   
+    
+//    public static void main(String[] args) throws Exception {
+//    	String userName = "itsayazmaqbool@gmail.com";
+//    	EmailForSignup.sendEmail(userName);
+//    }
 }
