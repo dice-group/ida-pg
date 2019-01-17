9f67f9f4a184dd807f5af16797f4500be7ca2886
Ayaz Maqbool <ayazmaqbooluet@gmail.com>
2019-01-17 20:04:13 +0100

    worked on the user controller as per the requirements changed in the user experience

--- poc-springsecurity-login/src/main/java/com/poc/services/UserService.java
+++ poc-springsecurity-login/src/main/java/com/poc/services/UserService.java
@@ -1,21 +1,17 @@
 package com.poc.services;
 
-import com.poc.commands.UserForm;
 import com.poc.model.User;
-
 import java.util.List;
 
 public interface UserService {
 
-    List<User> listAll();
+    public List<User> listAllUsers();
 
-    User getById(Long id);
+    public User getById(Long id);
 
-    User saveOrUpdate(User user);
+    public User saveOrUpdate(User user);
 
-    void delete(Long id);
+    public void delete(Long id);
 
-    User saveOrUpdateProductForm(UserForm userForm);
-
-	User getByUsername(String username);
+    public User getByUsername(String username);
 }
