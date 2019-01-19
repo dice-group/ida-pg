d6a70209f8b8fc49dedcb0cb60246d604b687d11
Ayaz Maqbool <ayazmaqbooluet@gmail.com>
2019-01-19 20:01:37 +0100

    added the encryption code for password

--- ida-ws/src/main/java/upb/ida/security/CustomAuthenticationProvider.java
+++ ida-ws/src/main/java/upb/ida/security/CustomAuthenticationProvider.java
@@ -4,12 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import upb.ida.bean.User;
@@ -21,6 +24,11 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
 	@Autowired
 	private UserService userService;
 	
+	@Bean
+	public PasswordEncoder encoder() {
+	    return new BCryptPasswordEncoder();
+	}
+	
     @Override
     public Authentication authenticate(Authentication authentication) 
       throws AuthenticationException {
@@ -31,7 +39,7 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
         User currentUser = userService.getByUsername(name);
        
         if (currentUser == null) return null;
-        if(currentUser.getUsername().equals(name) && currentUser.getPassword().equals(password)) {//shouldAuthenticateAgainstThirdPartySystem()) {
+        if(currentUser.getUsername().equals(name) && encoder().matches(password, currentUser.getPassword())) {
   
             List<GrantedAuthority> grantedAuths = new ArrayList<>();
             if(currentUser.getUserRole().equals("ADMIN"))
