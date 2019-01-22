16b057a7c9a941b01db31a825a755a58ee79e382
Ayaz Maqbool <ayazmaqbooluet@gmail.com>
2019-01-22 01:13:00 +0100

    resolved the codacy issues that were the cause for the build failure

--- ida-ws/src/main/java/upb/ida/security/CustomLogoutSuccessHandler.java
+++ ida-ws/src/main/java/upb/ida/security/CustomLogoutSuccessHandler.java
@@ -6,9 +6,7 @@ import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.security.core.Authentication;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 import org.springframework.stereotype.Component;
 
