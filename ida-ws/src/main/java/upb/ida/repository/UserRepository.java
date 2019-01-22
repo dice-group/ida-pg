d3fd76934ea155233667fd770ce23baa08809289
Ayaz Maqbool <ayazmaqbooluet@gmail.com>
2019-01-22 01:28:29 +0100

    resolving the issue for build failure on travis

--- ida-ws/src/main/java/upb/ida/repository/UserRepository.java
+++ ida-ws/src/main/java/upb/ida/repository/UserRepository.java
@@ -4,7 +4,9 @@ import upb.ida.bean.User;
 import org.springframework.data.neo4j.annotation.Query;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends Neo4jRepository<User, Long> {
 	
     @Query("MATCH (n:User {username: {username}}) RETURN n")
