76fcc0edee6d04201426abe51cd039cc72e13fa3
anu-muniyapla <anumuniyapla7395@gmail.com>
2019-01-24 07:57:28 +0100

    message rest controller test for bar graph

---
+++ ida-ws/src/test/java/upb/ida/test/BgMsgRestControllerTest.java
@@ -0,0 +1,115 @@
+package upb.ida.test;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.validation.constraints.Null;
+
+import org.json.JSONArray;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import net.minidev.json.JSONObject;
+import upb.ida.Application;
+import upb.ida.bean.ResponseBean;
+import upb.ida.rest.MessageRestController;
+import upb.ida.util.BarGraphUtil;
+
+@RunWith(SpringRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(classes = { Application.class })
+
+public class BgMsgRestControllerTest {
+
+	@Autowired
+	MessageRestController msgRstCntrl;
+	@Autowired
+	BarGraphUtil barGraph;
+
+	@Test
+	public void sendmessagetestPos() throws Exception {
+		ResponseBean responseBean;
+		responseBean = msgRstCntrl.sendmessage("I want a bar-graph visualisation for the current table", "1",
+				"movehubcostofliving.csv", "city");
+		responseBean = msgRstCntrl.sendmessage("x-axis is city", "1", "movehubcostofliving.csv", "city2");
+		responseBean = msgRstCntrl.sendmessage("y-axis is wine", "1", "movehubcostofliving.csv", "city2");
+		responseBean = msgRstCntrl.sendmessage("Top 2 records, sorted descending on wine", "1",
+				"movehubcostofliving.csv", "city2");
+
+		System.out.println(responseBean.getPayload().get("bgData"));
+
+		// System.out.println(actualMap);
+
+		// List<Map<String, String>> inputList = new ArrayList<>();
+		//
+		// HashMap<String, String> mapE1 = new HashMap<String, String>();
+		// mapE1.put("city", "nashville");
+		// mapE1.put("wine", "13.5");
+		//
+		// HashMap<String, String> mapE2 = new HashMap<String, String>();
+		// mapE2.put("city", "stavanger");
+		// mapE2.put("wine", "13.44");
+		//
+		// inputList.add(mapE1);
+		// inputList.add(mapE2);
+		
+		ObjectMapper mapper = new ObjectMapper();
+		ArrayNode jArray = mapper.createArrayNode();
+
+		ObjectNode user1 = mapper.createObjectNode();
+		user1.put("Wine", 13.5);
+		user1.put("City", "Nashville");
+
+		ObjectNode user2 = mapper.createObjectNode();
+		user2.put("Wine", 13.44);
+		user2.put("City", "Stavanger");
+		
+		jArray.add(user1);
+		jArray.add(user2);
+
+		System.out.print(jArray);
+		// Object keys = "city";
+		List<String> keys = new ArrayList<String>();
+	
+		keys.add("City");
+	
+		Map<String, Object> expected = new HashMap<String, Object>();
+
+		expected.put("xaxisname", "City");
+		expected.put("keys", keys);
+		 expected.put("baritems", jArray);
+		 expected.put("yaxisname", "Wine");
+		 System.out.println(expected);
+
+		// String xKey = "City";
+		// String yKey = "wine";
+		
+		//
+		// dataMap.put("xaxisname", xKey);
+		// dataMap.put("yaxisname", yKey);
+		// dataMap.put("keys", keys);
+		// dataMap.put("barItems", inputList);
+		// System.out.println(dataMap);
+
+		// barGraph.newJsonObjct("city", "wine", inputList);
+
+		@SuppressWarnings("unchecked")
+		Map<String, Object> actual = (Map<String, Object>) responseBean.getPayload().get("bgData");
+		assertEquals(expected, actual);
+
+	}
+
+}
