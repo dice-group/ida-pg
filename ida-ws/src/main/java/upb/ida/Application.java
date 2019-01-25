071016316224a25a19365fa97b3da8a2c4e1b72a
Ayaz Maqbool <ayazmaqbooluet@gmail.com>
2019-01-25 15:13:19 +0100

    resolving the session factory neo4j issue for travis build failure

--- ida-ws/src/main/java/upb/ida/Application.java
+++ ida-ws/src/main/java/upb/ida/Application.java
@@ -2,14 +2,18 @@ package upb.ida;
 
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @EnableNeo4jRepositories("upb.ida.repository")
-@EntityScan("upb.ida.bean.User")
+@EntityScan(basePackages = "upb.ida.domains")
+@EnableTransactionManagement
+@EnableAutoConfiguration
 @ImportResource({
 	"classpath:config/bean-config.xml",
 	"classpath:config/startup-config.xml"
