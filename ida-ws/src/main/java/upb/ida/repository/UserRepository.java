f4d5ca704199872d6954667911c3773687fd41a9
Ayaz Maqbool <ayazmaqbooluet@gmail.com>
2019-01-25 14:33:09 +0100

    workign on the travis build issue

--- ida-ws/src/main/java/upb/ida/repository/UserRepository.java
+++ ida-ws/src/main/java/upb/ida/repository/UserRepository.java
@@ -1,6 +1,6 @@
 package upb.ida.repository;
 
-import upb.ida.bean.User;
+import upb.ida.domains.User;
 import org.springframework.data.neo4j.annotation.Query;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.data.repository.query.Param;
--- ida-ws/src/main/java/upb/ida/rest/LoginController.java
+++ ida-ws/src/main/java/upb/ida/rest/LoginController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import upb.ida.bean.ResponseBean;
-import upb.ida.bean.User;
+import upb.ida.domains.User;
 import upb.ida.constant.IDALiteral;
 import upb.ida.service.UserService;
 
