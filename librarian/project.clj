d2cbdd895236cf93b0a0e981b88e9355f3ad0a0b
Clemens Damke <admin@cortys.de>
2019-02-03 19:37:41 +0100

    integrated the modularized librarian into ida-ws

--- librarian/project.clj
+++ librarian/project.clj
@@ -4,6 +4,7 @@
 
   :plugins [[lein-modules "0.3.11"]]
   :middleware [lein-modules.plugin/middleware]
+  :dependencies [[org.clojure/clojure]]
 
   :modules {:versions {org.clojure/clojure "1.10.0"
                        org.clojure/tools.logging "0.4.1"
@@ -12,4 +13,19 @@
 
                        librarian/helpers "1.0.0-SNAPSHOT"
                        librarian/model "1.0.0-SNAPSHOT"
-                       librarian/scraper "1.0.0-SNAPSHOT"}})
+                       librarian/scraper "1.0.0-SNAPSHOT"}
+
+            :inherited {:pom-plugins [[com.theoryinpractise/clojure-maven-plugin "1.8.1"
+                                       [:extensions true
+                                        :configuration ([:sourceDirectories [:sourceDirectory "src"]]
+                                                        [:testSourceDirectories [:testSourceDirectory "test"]])
+                                        :executions [:execution ([:goals [:goal "compile"]]
+                                                                 [:phase "compile"])]]]]}}
+
+  :aliases {"pom" ["modules" "pom"]} ; lein pom should only update the submodule poms
+
+  :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.11"]
+                                  [proto-repl "0.3.1"]
+                                  [proto-repl-charts "0.3.1"]
+                                  [proto-repl-sayid "0.1.3"]]
+                   :eastwood {:exclude-linters []}}})
