96602d10a3f758e13ee60737c6033e5ed824d801
Clemens Damke <admin@cortys.de>
2019-05-27 20:57:10 +0200

    datatypes are now propagated to completed call params and results

--- librarian/modules/generator/src/librarian/generator/core.clj
+++ librarian/modules/generator/src/librarian/generator/core.clj
@@ -214,47 +214,65 @@
    This tx will add the given params to a given call.
    Preexisting io-containers in call are detected via lookup-io.
    If an io-container is found in call, it will be replaced; if not, a new io-container will be created."
-  [call-io-type call-io->io-attr call->call-io-attr call lookup-io]
-  (mapcat (fn [io]
-            (let [call-io (lookup-io io)]
-              (if (some? call-io)
-                [[:db/add call-io call-io->io-attr io]]
+  [call-io-type call-io->io-attr call->call-io-attr]
+  (fn [call io->call-io-data]
+    (mapcat (fn [{io :db/id, types :types}]
+              (if-some [{call-io :db/id, {:keys [semantic full]} :receivers} (io->call-io-data io)]
+                (into [[:db/add call-io call-io->io-attr io]]
+                      (mapcat #(eduction (map (fn [rec] [:db/add rec ::typed/datatype (:db/id %)]))
+                                         (if (:semantic %) semantic full)))
+                      types)
                 (let [call-io (d/tempid nil)]
                   [{:db/id call-io
                     :type call-io-type
-                    call-io->io-attr io}
+                    call-io->io-attr io
+                    ::typed/datatype (map :db/id types)}
                    [:db/add call call->call-io-attr call-io]]))))))
+
+(def param->tx (io-conts->tx ::call-parameter/call-parameter
+                             ::call-parameter/parameter
+                             ::call/parameter))
+
+(def result->tx (io-conts->tx ::call-result/call-result
+                              ::call-result/result
+                              ::call/result))
+
+(defn call->io-conts
+  [db call io-type]
+  (mapv (fn [p]
+          (let [pid (:v p)]
+            {:db/id pid
+             :types (gq/types db pid)}))
+       (d/datoms db :eavt call io-type)))
 
 (defn call-completion-actions
   [{:keys [db placeholder-matches]} flaw]
   (let [callable (:v (first (d/datoms db :eavt flaw ::call/callable)))
         e (d/entity db flaw)
-        p->cp (into {} (map (fn [p] [(-> p ::call-parameter/parameter :db/id) (:db/id p)]))
+        p->cp (into {} (map (fn [p] [(-> p ::call-parameter/parameter :db/id)
+                                     (let [id (:db/id p)]
+                                       {:db/id id
+                                        :receivers (gq/receivers db id)})]))
                     (::call/parameter e))
-        r->cr (into {} (map (fn [r] [(-> r ::call-result/result :db/id) (:db/id r)]))
+        r->cr (into {} (map (fn [r] [(-> r ::call-result/result :db/id)
+                                     (let [id (:db/id r)]
+                                       {:db/id id
+                                        :receivers (gq/receivers db id)})]))
                     (::call/result e))
         completions (placeholder-matches db callable)]
     (mapcat (fn [{match :match
                   matched-params ::callable/parameter
                   matched-results ::callable/result}]
-              (let [params (map :v (d/datoms db :eavt match ::callable/parameter))
-                    results (map :v (d/datoms db :eavt match ::callable/result))
+              (let [params (call->io-conts db match ::callable/parameter)
+                    results (call->io-conts db match ::callable/result)
                     params-combos (all-valid-call-io-match-combos matched-params)
                     results-combos (all-valid-call-io-match-combos matched-results)]
                 (for [params-combo params-combos
                       results-combo results-combos]
                   (let [tx (-> (transient [[:db/add flaw ::call/callable match]])
-                               (into! (io-conts->tx ::call-parameter/call-parameter
-                                                    ::call-parameter/parameter
-                                                    ::call/parameter
-                                                    flaw
-                                                    (comp p->cp params-combo))
+                               (into! (param->tx flaw (comp p->cp params-combo))
                                       params)
-                               (into! (io-conts->tx ::call-result/call-result
-                                                    ::call-result/result
-                                                    ::call/result
-                                                    flaw
-                                                    (comp r->cr results-combo))
+                               (into! (result->tx flaw (comp r->cr results-combo))
                                       results)
                                (persistent!))]
                     {:cost 0
