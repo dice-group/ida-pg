27a00078950aef2fc24d7d0cbd854f5ec4d77362
Deepak <erdkgarg08@gmail.com>
2019-06-09 15:56:54 +0200

    code optimisation

--- ida-ws/src/main/java/upb/ida/security/CustomAuthenticationProvider.java
+++ ida-ws/src/main/java/upb/ida/security/CustomAuthenticationProvider.java
@@ -18,48 +18,46 @@ import upb.ida.service.UserService;
 
 @Component
 public class CustomAuthenticationProvider implements AuthenticationProvider {
- 	
-    @Override
-    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-    	
-    	//name is username here
-        String name = authentication.getName();
-        String password = authentication.getCredentials().toString();
-        
-        System.out.println("password from authentication before hashing"+password);
-        try {
+
+	@Override
+	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+
+		// name is user name here
+		String name = authentication.getName();
+		String password = authentication.getCredentials().toString();
+
+		System.out.println("password from authentication before hashing" + password);
+		try {
 			password = UserController.hashPassword(password);
 		} catch (NoSuchAlgorithmException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-        System.out.println("password from authentication after hashing"+password);
-        
-        System.out.println("username entered by user   "+name);
-        System.out.println("password entered by user    "+password);        
-        
-       // User currentUser = userService.getByUsername(name);
-        User currentUser = UserService.getByUsername(name); 
-        System.out.println("username databse "+currentUser.getUsername());
-        System.out.println("password database by user    "+currentUser.getPassword());        
-       
-        if(currentUser.getUsername().equals(name) && currentUser.getPassword().equals(password)) {
-        	
-            List<GrantedAuthority> grantedAuths = new ArrayList<>();
-            if(currentUser.getUserRole().equals("ADMIN"))
-            	grantedAuths.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
-            grantedAuths.add(new SimpleGrantedAuthority("ROLE_USER"));
-            
-            return new UsernamePasswordAuthenticationToken(
-              name, password, grantedAuths);
-        } else {
-        	return null;
-        }
-    }
- 
-    @Override
-    public boolean supports(Class<?> authentication) {
-        return authentication.equals(
-          UsernamePasswordAuthenticationToken.class);
-    }
+		System.out.println("password from authentication after hashing" + password);
+
+		System.out.println("username entered by user   " + name);
+		System.out.println("password entered by user    " + password);
+
+		// User currentUser = userService.getByUsername(name);
+		User currentUser = UserService.getByUsername(name);
+		System.out.println("username databse " + currentUser.getUsername());
+		System.out.println("password database by user    " + currentUser.getPassword());
+
+		if (currentUser.getUsername().equals(name) && currentUser.getPassword().equals(password)) {
+
+			List<GrantedAuthority> grantedAuths = new ArrayList<>();
+			if (currentUser.getUserRole().equals("ADMIN"))
+				grantedAuths.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
+			grantedAuths.add(new SimpleGrantedAuthority("ROLE_USER"));
+
+			return new UsernamePasswordAuthenticationToken(name, password, grantedAuths);
+		} else {
+			return null;
+		}
+	}
+
+	@Override
+	public boolean supports(Class<?> authentication) {
+		return authentication.equals(UsernamePasswordAuthenticationToken.class);
+	}
 }
