7d2e34ed1af2cfa20e2015b2857404d07ab821dd
Deepak <erdkgarg08@gmail.com>
2019-06-09 15:42:01 +0200

    authentication problem resolved and code optimisation

--- ida-ws/src/main/java/upb/ida/security/CustomAuthenticationProvider.java
+++ ida-ws/src/main/java/upb/ida/security/CustomAuthenticationProvider.java
@@ -4,16 +4,12 @@ import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import upb.ida.domains.User;
@@ -22,15 +18,7 @@ import upb.ida.service.UserService;
 
 @Component
 public class CustomAuthenticationProvider implements AuthenticationProvider {
- 
-	@Autowired
-	private UserService userService;
-	
-//	@Bean
-//	public PasswordEncoder encoder() {
-//	    return new BCryptPasswordEncoder();
-//	}
-	
+ 	
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
     	
@@ -47,19 +35,16 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
 		}
         System.out.println("password from authentication after hashing"+password);
         
+        System.out.println("username entered by user   "+name);
+        System.out.println("password entered by user    "+password);        
+        
        // User currentUser = userService.getByUsername(name);
         User currentUser = UserService.getByUsername(name); 
-        try {
-        	password = UserController.hashPassword(password);
-		} catch (NoSuchAlgorithmException e) {
-			
-			e.printStackTrace();
-		}
+        System.out.println("username databse "+currentUser.getUsername());
+        System.out.println("password database by user    "+currentUser.getPassword());        
        
-        if (currentUser == null) return null;
         if(currentUser.getUsername().equals(name) && currentUser.getPassword().equals(password)) {
-       // if(currentUser.getUsername().equals(name) && encoder().matches(password, currentUser.getPassword())) {
-  
+        	
             List<GrantedAuthority> grantedAuths = new ArrayList<>();
             if(currentUser.getUserRole().equals("ADMIN"))
             	grantedAuths.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
@@ -68,8 +53,7 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
             return new UsernamePasswordAuthenticationToken(
               name, password, grantedAuths);
         } else {
-        	authentication.setAuthenticated(false);
-            return authentication;
+        	return null;
         }
     }
  
