1b8ff64a68ed0036af637b1b0564f0ee7238cd39
RahulSherikar <rahulsherikar94@gmail.com>
2019-06-25 16:06:34 +0200

    docker thing

--- ida-chatbot/src/app/app.module.ts
+++ ida-chatbot/src/app/app.module.ts
@@ -61,7 +61,7 @@ import { SsbViewComponent } from './components/ssb-view/ssb-view.component';
 import { VennViewComponent } from './components/venn-view/venn-view.component';
 import { DeckglHexViewComponent } from './components/deckgl-hex-view/deckgl-hex-view.component';
 import { RdfOntologyViewComponent } from './components/rdf-ontology-view/rdf-ontology-view.component';
-//import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -123,8 +123,8 @@ import { RdfOntologyViewComponent } from './components/rdf-ontology-view/rdf-ont
     MatTabsModule,
     MatToolbarModule,
     MatTooltipModule,
-    FlexLayoutModule//,
-   // NgbModule
+    FlexLayoutModule,
+   NgbModule
   ],
   providers: [{
     provide: HTTP_INTERCEPTORS,
