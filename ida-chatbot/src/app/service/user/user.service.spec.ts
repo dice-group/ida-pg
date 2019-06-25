96ffc34344d30c9264d904174c3188e26b78eb5a
Ali AZmi <aliazmi68@gmail.com>
2019-06-26 01:17:08 +0200

    Minor changes

--- ida-chatbot/src/app/service/user/user.service.spec.ts
+++ ida-chatbot/src/app/service/user/user.service.spec.ts
@@ -1,12 +1,12 @@
-// import { TestBed } from '@angular/core/testing';
-//
-// import { UserService } from './user.service';
-//
-// describe('UserService', () => {
-//   beforeEach(() => TestBed.configureTestingModule({}));
-//
-//   it('should be created', () => {
-//     const service: UserService = TestBed.get(UserService);
-//     expect(service).toBeTruthy();
-//   });
-// });
+import { TestBed } from '@angular/core/testing';
+
+import { UserService } from './user.service';
+
+describe('UserService', () => {
+ beforeEach(() => TestBed.configureTestingModule({}));
+
+  it('should be created', () => {
+   const service: UserService = TestBed.get(UserService);
+    expect(service).toBeTruthy();
+  });
+});
