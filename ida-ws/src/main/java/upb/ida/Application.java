f35c519e6eb6dc1db3f56db38c3a34416348234d
Ali AZmi <aliazmi68@gmail.com>
2019-06-29 22:57:16 +0200

    Revert "Minor changes"
    This reverts commit 38a65b6a551157f797489009dfba9d421ab96aa8, reversing
    changes made to 96bb585131e4ab7e4a8c9a4a535d9c7164b0b947.

--- ida-ws/src/main/java/upb/ida/Application.java
+++ ida-ws/src/main/java/upb/ida/Application.java
@@ -1,18 +1,11 @@
 package upb.ida;
 
-//import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
-//import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-//@EnableNeo4jRepositories("upb.ida.repository")
-//@EntityScan(basePackages = "upb.ida.domains")
-//@EnableTransactionManagement
-@EnableAutoConfiguration
 @ImportResource({
 	"classpath:config/bean-config.xml",
 	"classpath:config/startup-config.xml"
