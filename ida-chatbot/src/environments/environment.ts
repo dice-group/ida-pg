a27a37272ef52241a4df6b2e0b9d04922e7bdd10
Ali AZmi <aliazmi68@gmail.com>
2019-07-02 01:19:03 +0200

    Minor changes

--- ida-chatbot/src/environments/environment.ts
+++ ida-chatbot/src/environments/environment.ts
@@ -1,4 +1,7 @@
 export const environment = {
   production: false,
-  apiBase: 'http://localhost:8080/ida-ws'
+  // apiBase: 'http://localhost:8080/ida-ws',
+  apiBase: 'http://127.0.0.1:3080/ida-ws',
+  // chatbotBase: 'http://localhost:4200'
+  chatbotBase: 'http://127.0.0.1:3080'
 };
