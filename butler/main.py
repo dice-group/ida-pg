614b58765618743a2b4c46174cd950e0b04f3653
Maqbool ur Rahim Khan <maqboolurrahim@gmail.com>
2019-07-07 14:55:39 +0200

    context added

--- butler/main.py
+++ butler/main.py
@@ -1,19 +1,25 @@
 import fasttext
 import flask
 from flask import request
+from config_loader import config_loader
 
 app = flask.Flask(__name__)
 
-corpus = 'data.db'
+print('Loading intent configuration file.. ', end='')
+intent_config = config_loader('intent_config.json')
+print('Done')
 
+print('Initializing classifier.. ', end='')
+corpus = 'data.db'
 classifier = fasttext.supervised(corpus, 'model_chat', label_prefix='__label__')
+print('Done')
 
 # Api to classify the user message.
 @app.route('/predict', methods=['GET'])
 def index():
     # Getting the user message from url parameter.
-    query = request.args.get('msg')
-    if query is None:
+    msg = request.args.get('msg')
+    if msg is None:
         return flask.jsonify({
             'error': 'No query parameter'
         })
@@ -23,21 +29,39 @@ def index():
     response = {}
 
     # Intent classification of message
-    cls = classifier.predict([query])
-    cls = cls[0][0]
+    cls = classifier.predict([msg])[0][0]
+    print(cls)
 
-    # set the reply based on the class
-    if cls == 'greet':
-        response['message'] = 'Hello'
-    elif cls == 'help':
-        response['message'] = 'I can load datasets for you. \n I can visualise data too.'
-    elif cls == 'dataset':
-        response['message'] = 'Dataset loaded.'
-    elif cls == 'barchart':
-        response['message'] = 'Barchart drawn.'
+    if cls in intent_config:
+        if 'parameters' in intent_config.get(cls):
+            parameters = intent_config.get(cls).get('parameters')
+
+            if 'param' not in request.args:
+                response['param'] = 0
+            else:
+                response['param'] += 1
+
+            param = parameters[response.get('param')]
+            for k in param:
+                response['reply'] = param.get(k)
+
+            response['complete'] = 0
+            response['intent'] = cls
+        else:
+            response['complete'] = 1
+            if cls == 'greet':
+                response['reply'] = 'Hello'
+            elif cls == 'help':
+                response['reply'] = 'I can load datasets for you. \n I can visualise data too.'
+            elif cls == 'dataset':
+                response['reply'] = 'Dataset loaded.'
+            elif cls == 'barchart':
+                response['reply'] = 'Barchart drawn.'
+            print('parameters missing')
 
     # returning the response object as json
     return flask.jsonify(response)
 
 
+print('Starting server')
 app.run()
