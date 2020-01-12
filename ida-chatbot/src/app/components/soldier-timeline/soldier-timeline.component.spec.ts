faf175d7130da39d415c7bc6fbd6ffdcdd68bb70
RahulSherikar <rahulsherikar94@gmail.com>
2020-01-12 18:57:43 +0100

    resolving "Codacy/PR Quality Review" issues.

--- ida-chatbot/src/app/components/soldier-timeline/soldier-timeline.component.spec.ts
+++ ida-chatbot/src/app/components/soldier-timeline/soldier-timeline.component.spec.ts
@@ -1,25 +1,25 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing'
 
-import { SoldierTimelineComponent } from './soldier-timeline.component';
+import { SoldierTimelineComponent } from './soldier-timeline.component'
 
 describe('SoldierTimelineComponent', () => {
-  let component: SoldierTimelineComponent;
-  let fixture: ComponentFixture<SoldierTimelineComponent>;
+  let component: SoldierTimelineComponent
+  let fixture: ComponentFixture<SoldierTimelineComponent>
 
   beforeEach(async(() => { 
     TestBed.configureTestingModule({
       declarations: [ SoldierTimelineComponent ]
     })
-    .compileComponents();
-  }));
+    .compileComponents()
+  }))
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(SoldierTimelineComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    fixture = TestBed.createComponent(SoldierTimelineComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    expect(component).toBeTruthy()
+  })
+})
