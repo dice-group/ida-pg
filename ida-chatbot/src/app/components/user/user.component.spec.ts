fa1c098d7f779862b00bb2ebcd33045ab9329978
Nandeesh Patel <nandeeshpatelgp@gmail.com>
2020-01-14 21:18:56 +0100

    Resolving UI codacy issues.

--- ida-chatbot/src/app/components/user/user.component.spec.ts
+++ ida-chatbot/src/app/components/user/user.component.spec.ts
@@ -1,25 +1,25 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing'
 
-import { UserComponent } from './user.component';
+import { UserComponent } from './user.component'
 
 describe('UserComponent', () => {
-  let component: UserComponent;
-  let fixture: ComponentFixture<UserComponent>;
+  let component: UserComponent
+  let fixture: ComponentFixture<UserComponent>
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ UserComponent ]
     })
-    .compileComponents();
-  }));
+    .compileComponents()
+  }))
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(UserComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    fixture = TestBed.createComponent(UserComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    expect(component).toBeTruthy()
+  })
 });
