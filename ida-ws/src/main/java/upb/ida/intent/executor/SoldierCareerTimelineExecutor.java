5790fc3fd9e349fcb2c7c66c7f30fb0b97df9728
toardik <hardik.topiwala@gmail.com>
2020-01-18 15:40:34 +0100

    Load dataset & soldier career timeline intent implementation

---
+++ ida-ws/src/main/java/upb/ida/intent/executor/SoldierCareerTimelineExecutor.java
@@ -0,0 +1,38 @@
+package upb.ida.intent.executor;
+
+import upb.ida.bean.ResponseBean;
+import upb.ida.dao.SoldierTimeLine;
+import upb.ida.intent.AnswerHandlingStrategy;
+import upb.ida.intent.exception.IntentException;
+import upb.ida.intent.model.ChatbotContext;
+import upb.ida.intent.model.Question;
+import upb.ida.util.BeanUtil;
+
+import java.util.Collections;
+import java.util.Map;
+
+public class SoldierCareerTimelineExecutor extends AbstractExecutor implements IntentExecutor {
+
+	public SoldierCareerTimelineExecutor() {
+		super(Collections.unmodifiableList(
+				Collections.singletonList(
+						new Question("What is the soldier id?", Collections.singletonList("soldier-id"), "Showing career soldier for soldier id : ${soldier-id}.", AnswerHandlingStrategy.NUMERIC_VALUE, false)
+				))
+		);
+
+	}
+
+
+	@Override
+	public void execute(ChatbotContext context) throws IntentException {
+		SoldierTimeLine soldierTimeLine = new SoldierTimeLine("ssfuehrer", false);
+		ResponseBean responseBean = BeanUtil.getBean(ResponseBean.class);
+
+		Map<String, String> savedAnswers = context.getSavedAnswers();
+		String soldierId = savedAnswers.get("soldier-id");
+		Map<String, Map<String, String>> data = soldierTimeLine.getData(soldierId);
+		responseBean.getPayload().put("soldierTimeLineData", data);
+		responseBean.setActnCode(10);
+		context.resetOnNextRequest();
+	}
+}
